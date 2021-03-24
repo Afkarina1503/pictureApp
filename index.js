655c37036cbac82466672afea23cc91ca25e51e6
Afkarina1503 <afkarina1503@gmail.com>
2021-03-24 18:21:46 +0700

    pictureApp

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import login from'./src/login';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => login);
---
+++ src/login.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View,TouchableOpacity,TextInput,Image} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default class CoreComponen extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            Username:"",
+            Password:'',
+        };
+        
+    }
+  render() {
+    return (
+        <View style={styles.bg1}>
+ 
+        <View >
+            <Text style={styles.text2}>PictureApp</Text>
+            <View>  
+            <Image 
+            style={styles.img}
+            source={require('../src/album/camera.png')} />
+            </View>    
+        
+        <View style ={styles.container}>   
+         
+        <TextInput 
+            onChangeText ={(Username)=>this.setState({Username})}
+            value={this.state.Username}
+            placeholder='Username' 
+            style={styles.formInput}
+        />
+        <TextInput 
+            onChangeText={(Password)=>this.setState({Password})}
+            value={this.state.Email}
+            placeholder='Password' 
+            style={styles.formInput}
+        />   
+         <TouchableOpacity 
+         style ={styles.button}  > 
+            <Text style={styles.text1}>Login</Text>
+        </TouchableOpacity>
+        <View>
+            <Text style={styles.text5}>Sign Up</Text>
+            </View>
+      </View>
+      </View>
+      </View>
+     
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    bg1:{
+     flex:1,
+     backgroundColor:'white',
+    },
+    container:{
+     paddingHorizontal:18,
+     marginVertical:20,
+
+
+    },
+    formInput:{
+        height:50,
+        borderWidth:1,
+        borderColor:'grey',
+        borderRadius:18,
+        padding:15,
+        marginTop:15,
+        
+    },
+    button:{
+        height:50,
+        width:150,
+        backgroundColor:'#C4C4C4',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:18,
+        marginVertical:15,
+        marginHorizontal:85,
+    },
+    text1:{
+        fontSize:25,
+        fontWeight:'bold',
+        color:'white',
+    },
+    text2:{
+        fontSize:40,
+        color:'black',
+        paddingTop:50,
+        textAlign:'center',
+        fontFamily:'Poppins',
+        
+
+    },
+    text5:{
+        fontSize:20,
+        marginTop:-2,
+        marginLeft:129,
+        textDecorationLine:'underline',        
+
+    },
+    img:{
+        width:150,
+        height:150,
+        marginHorizontal:100,
+        marginVertical:30,
+        
+    },
+
+});
